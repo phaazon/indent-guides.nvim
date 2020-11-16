efa5a4968648c6cfcbe08a88d0a98dc99779917f
glepnir <glepnir@gopherhub.org>
2020-11-16 21:45:53 +0800

    update

--- lua/indent_guides.lua
+++ lua/indent_guides.lua
@@ -1,57 +1,52 @@
 local M = {}
 local vim,api = vim,vim.api
 
-M.default_opts = {
+M.options = {
     indent_levels = 30;
-    indent_guide_size = 1;
+    indent_guide_size = 0;
     indent_start_level = 1;
     indent_space_guides = true;
     indent_tab_guides = false;
     indent_pretty_guides = false;
     indent_soft_pattern = '\\s';
     exclude_filetypes = {'help','dashboard','terminal'};
-    -- TODO add rainbow mode support just like vscode
-    indent_rainbow_mode = false;
 }
 
 local indent_get_matches = function()
   local has_matches,matches = pcall(api.nvim_win_get_var,0,'indent_guides_matches')
   if has_matches then
     return matches
-  else
-    return {}
   end
+  return {}
 end
 
 local indent_clear_matches = function()
   local matches = indent_get_matches()
   if next(matches) ~= nil then
-    for idx,match_id in pairs(matches) do
+    for _,match_id in ipairs(matches) do
       vim.fn.matchdelete(match_id)
-      table.remove(matches,idx)
     end
   end
+  api.nvim_win_del_var(0,'indent_guides_matches')
 end
 
 local indent_highlight_color =function ()
-  if  not M.default_opts['indent_rainbow_mode'] then
-    local even = {'#2E323A','#34383F'}
-    local odd = {'#34383F','#2E323A'}
-    api.nvim_command('hi IndentGuidesEven guifg=' .. even[1] .. ' guibg='.. even[2])
-    api.nvim_command('hi IndentGuidesOdd guifg=' .. odd[1] .. ' guibg='.. odd[2])
-  end
+  local even = {'#2E323A','#34383F'}
+  local odd = {'#34383F','#2E323A'}
+  api.nvim_command('hi IndentGuidesEven guifg=' .. even[1] .. ' guibg='.. even[2])
+  api.nvim_command('hi IndentGuidesOdd guifg=' .. odd[1] .. ' guibg='.. odd[2])
 end
 
 local indent_highlight_pattern= function(indent_pattern,column_start,indent_size)
-  local pattern
-  pattern = '^' .. indent_pattern .. '*\\%' .. column_start .. 'v\\zs'
+  local pattern = '^'
+  pattern = pattern .. indent_pattern .. '*\\%' .. column_start .. 'v\\zs'
   pattern = pattern .. indent_pattern .. '*\\%' .. (column_start + indent_size) .. 'v'
   pattern = pattern .. '\\ze'
   return pattern
 end
 
 local has_value = function(tbl,val)
-  for _,v in pairs(tbl) do
+  for _,v in ipairs(tbl) do
     if v == val then
       return true
     end
@@ -60,7 +55,7 @@ local has_value = function(tbl,val)
 end
 
 local indent_guides_enable = function()
-  local new_opts = M.default_opts
+  local new_opts = M.options
   local buf_ft = vim.bo.filetype
 
   if has_value(new_opts.exclude_filetypes,buf_ft) then
@@ -80,12 +75,16 @@ local indent_guides_enable = function()
   end
 
   indent_highlight_color()
-  -- indent_clear_matches()
 
   local matches = indent_get_matches()
   local level_tbl = vim.fn.range(new_opts['indent_start_level'],new_opts['indent_levels'])
   for _,level in pairs(level_tbl) do
-    local group = 'IndentGuides' .. ((level % 2 == 0) and 'Even' or 'Odd')
+    local group = 'IndentGuides'
+    if level % 2 == 0 then
+      group = group .. 'Even'
+    else
+      group = group .. 'Odd'
+    end
     local column_start = (level -1 ) * indent_size + 1
 
     if new_opts['indent_space_guides']  then
@@ -114,14 +113,13 @@ end
 function M.indent_guides_disable()
   indent_enabled = false
   indent_clear_matches()
-  api.nvim_win_del_var(0,'indent_guides_matches')
   vim.api.nvim_command('augroup indent_guides_nvim')
   vim.api.nvim_command('autocmd!')
   vim.api.nvim_command('augroup END')
 end
 
 function  M.indent_guides_augroup()
-  local definition = {'BufEnter','WinEnter','FileType'}
+  local definition = {'BufRead','FileType'}
   vim.api.nvim_command('augroup indent_guides_nvim')
   vim.api.nvim_command('autocmd!')
   for _, def in ipairs(definition) do
