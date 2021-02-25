7e5375015bb912b9ef6829ee0e68d5cef1ffce5e
stephen <glepnir@gopherhub.org>
2021-02-25 16:00:06 +0800

    change color to black

--- lua/indent_guides.lua
+++ lua/indent_guides.lua
@@ -11,8 +11,8 @@ local get_default_options = function()
     indent_tab_guides = false;
     indent_soft_pattern = '\\s';
     exclude_filetypes = {'help','dashboard','dashpreview','NvimTree','vista','sagahover','sagasignature','packer'};
-    even_colors = { fg = '#2b3834',bg = '#2d3542' };
-    odd_colors = { fg = '#2d3542',bg = '#2b3834' };
+    even_colors = { fg = '#23272e',bg = '#23272e' };
+    odd_colors = { fg = '#23272e',bg = '#23272e' };
   }
   return default_opts
 end
